34ae2a454de056b48b20b65e3ab40f0d531e7906
Vikrant Varma <vikrant.varma94@gmail.com>
2014-05-22 21:06:05 +0530

    Install nodejs and mongodb

---
+++ install-nodejs.sh
@@ -0,0 +1,5 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
