c2cb00ca1e88a331f5c32f331448d1f24bbc33d3
Vikrant Varma <vikrant.varma94@gmail.com>
2014-05-22 21:56:44 +0530

    Don't symlink /usr/bin/node

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -2,4 +2,3 @@ sudo apt-get install -qq --force-yes software-properties-common python-software-
 echo | sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update -qq
 sudo apt-get install -qq --force-yes python-software-properties python g++ make nodejs
-sudo ln -s /usr/bin/nodejs /usr/bin/node
